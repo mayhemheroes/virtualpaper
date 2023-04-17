52773fe9380c10f5a3b4c99c4a8b46f9c01d9291
Brandon Watson <bmwtsn99@gmail.com>
2023-04-16 23:04:03 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzzvirtualpaper.go
@@ -0,0 +1,63 @@
+package fuzzvirtualpaper
+
+import (
+    "strconv"
+    fuzz "github.com/AdaLogics/go-fuzz-headers"
+
+    "tryffel.net/go/virtualpaper/process"
+)
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) > 2 {
+        num, _ = strconv.Atoi(string(bytes[0]))
+        bytes = bytes[1:]
+        fuzzConsumer := fuzz.NewConsumer(bytes)
+        
+        switch num {
+            case 2:
+                fuzzName, err := fuzzConsumer.GetString()
+                if err != nil {
+                    return 0
+                }
+
+                process.GetHash(fuzzName)
+                return 0
+
+            case 1:
+                fuzzId, err := fuzzConsumer.GetString()
+                if err != nil {
+                    return 0
+                }
+
+                process.DeleteDocument(fuzzId)
+                return 0
+
+            case 0:
+                fuzzName, err := fuzzConsumer.GetString()
+                if err != nil {
+                    return 0
+                }
+
+                process.MimeTypeFromName(fuzzName)
+                return 0
+        
+            default:
+                fuzzType, err := fuzzConsumer.GetString()
+                fuzzName, err := fuzzConsumer.GetString()
+                if err != nil {
+                    return 0
+                }
+
+                process.MimeTypeIsSupported(fuzzType, fuzzName)
+                return 0
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
